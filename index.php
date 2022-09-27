bb6359686e296a92539c16b5dd4f147e3a56c9fa
Melanie <melaniexiomara485@gmail.com>
2022-09-27 09:28:21 -0300

    Header

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-ndjkan
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/styleIndex.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="container">
+            <div class="btn-menu">
+                <label for="" class="icon-menu"></label>
+            </div>
+            <nav class="main-nav">
+                <div class="logo">
+                    <!-- <img width="100px" height="80px" src="https://www.actualidadmotor.com/wp-content/uploads/2012/03/embrague-1.jpg.webp"> -->
+                    <label>Raly Embrague</label>
+                </div>
+                <label><a href="#">Realizar venta</a></label>
+                <label><a href="#">Servicio</a></label>
+            </nav>
+        </div>
+    </header>
+
+
+</body>
+
+</html>
